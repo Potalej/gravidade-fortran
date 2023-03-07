60406b813fcb7dffb52760efd2605da6889f48e0
Potalej <oapotalej@gmail.com>
2023-03-07 14:56:42 -0300

    Condicionamento do momento angular total.

--- auxiliares/auxiliares.f90
+++ auxiliares/auxiliares.f90
@@ -9,7 +9,7 @@ module auxiliares
 
   implicit none
   private
-  public produto_vetorial
+  public produto_vetorial, tensor_inercia_geral, sistema_linear3
 
 contains
 
@@ -27,5 +27,72 @@ contains
     produto_vetorial(3) =  u(1)*v(2)-v(1)*u(2)
   
   end function produto_vetorial
+
+  ! determinante de uma matriz 3x3
+  real function determinante (M) result (det)
+
+    real, dimension(3,3), intent(in) :: M
+
+    det = M(1,1)*(M(2,2)*M(3,3)-M(3,2)*M(2,3)) - M(1,2)*(M(2,1)*M(3,3)-M(2,3)*M(3,1)) + M(1,3)*(M(2,1)*M(3,2)-M(2,2)*M(3,1))
+
+  end function determinante
+
+  ! tensor de inércia
+  function tensor_inercia (m, R)
+
+    implicit none
+    real, dimension(3), intent(in) :: R
+    real, intent(in)               :: m
+    real, dimension(3,3) :: tensor_inercia
+    integer              :: a, b
+
+    do a = 1, 3
+      do b = 1, 3
+        if (a /= b) then
+          tensor_inercia(a,b) = m * R(a) * R(b)
+        end if
+      end do 
+    end do
+
+    tensor_inercia(1,1) = - m * (R(2)**2 + R(3)**2)
+    tensor_inercia(2,2) = - m * (R(1)**2 + R(3)**2)
+    tensor_inercia(3,3) = - m * (R(1)**2 + R(2)**2)
+
+  end function tensor_inercia
+
+  function tensor_inercia_geral (massas, posicoes)
+
+    implicit none
+    real, dimension(:), intent(in) :: massas
+    integer :: a
+    real, dimension(size(massas),3) :: posicoes
+    real, dimension(3,3) :: tensor_inercia_geral
+
+    tensor_inercia_geral(:,:) = 0
+    
+    do a = 1, size(massas)
+      tensor_inercia_geral = tensor_inercia_geral + tensor_inercia(massas(a), posicoes(a,:))
+    end do   
+
+  end function tensor_inercia_geral
+
+  ! resolve um sistema de 3 equações lineares usando o método de Cramer
+  function sistema_linear3 (A, b)
+
+    implicit none
+    real, dimension(3,3), intent(in) :: A
+    real, dimension(3), intent(in)   :: b
+    real, dimension(3)               :: sistema_linear3
+    real                             :: detA
+
+    ! calcula a determinante de A
+    detA = determinante(A)
+
+    ! aplica o método
+    sistema_linear3(1) = (1/detA) * determinante((/b, A(2,:), A(3,:)/))
+    sistema_linear3(2) = (1/detA) * determinante((/A(1,:), b , A(3,:)/))
+    sistema_linear3(3) = (1/detA) * determinante((/A(1,:), A(2,:), b/))
+
+  end function
 
 end module auxiliares
---
+++ condicionamento/condicoesArtigo.f90
@@ -0,0 +1,108 @@
+module condicoesArtigo
+  use angular
+  use hamiltoniano
+  use auxiliares
+
+  implicit none
+  private
+  public condicionar, zerar_momentoAngular
+
+contains
+
+  function gerar_vetores3d (N, min, max)
+
+    integer, intent(in)     :: N, min, max
+    real, dimension(N,3)    :: gerar_vetores3d
+    integer, dimension(N,3) :: ajuste
+
+    ajuste(:,:) = min
+
+    call random_seed()
+    call random_number(gerar_vetores3d)
+
+    ! agora condiciona no intervalo
+    gerar_vetores3d = gerar_vetores3d * (max - min + 1) + ajuste
+
+  end function gerar_vetores3d
+
+  function gerar_massas (N, min, max)
+
+    integer, intent(in) :: N, min, max
+    real, dimension(N)  :: gerar_massas
+    integer          :: ajuste(N)
+
+    ajuste(:) = min
+
+    call random_seed()
+    call random_number(gerar_massas)
+
+    ! agora condiciona no intervalo
+    gerar_massas = gerar_massas * (max - min + 1) + ajuste
+
+  end function gerar_massas
+
+  subroutine gerarValores (N, massas, posicoes, momentos, minMassas, maxMassas, minPos, maxPos, minMom, maxMom)
+
+    implicit none
+    integer, intent(in)      :: N, minPos, maxPos, minMom, maxMom, minMassas, maxMassas
+    real, intent(inout) :: posicoes(N,3), momentos(N,3), massas(N)
+
+    ! gera massas
+    massas = gerar_massas(N, minMassas, maxMassas)
+
+    ! gera as posições
+    posicoes = gerar_vetores3d(N, minPos, maxPos)
+
+    ! gera os momentos
+    momentos = gerar_vetores3d(N, minMom, maxMom)
+
+  end subroutine gerarValores
+
+  ! zerando o momento angular
+  subroutine zerar_momentoAngular (massas, posicoes, momentos)
+    
+    implicit none
+    real, intent(inout)    :: posicoes(:,:), momentos(:,:), massas(:)
+    integer                :: a
+    real                :: momentoAngular_total(3), vetorRotacao(3), tensorInercia(3,3)
+
+    ! calcula o momento angular total
+    momentoAngular_total = angular_geral (posicoes,momentos)
+
+    ! calcular o tensor de inércia
+    tensorInercia = tensor_inercia_geral(massas, posicoes)
+
+    ! calcula o vetor de rotação (resolve sistema linear)
+    vetorRotacao = sistema_linear3 (tensorInercia, - momentoAngular_total)
+
+    ! percorre os corpos
+    do a = 1, size(posicoes,1)
+      ! produto vetorial da posição pelo vetor de rotação
+      momentos(a,:) = momentos(a,:) + massas(a) * produto_vetorial(posicoes(a,:), vetorRotacao)
+    end do
+
+  end subroutine zerar_momentoAngular
+
+
+  ! subroutine condicionar (N, minPos, maxPos, minMom, maxMom, minMassas, maxMassas)
+  subroutine condicionar (massas, posicoes, momentos)
+    implicit none
+
+    ! integer, intent(in)      :: N, minPos, maxPos, minMom, maxMom, minMassas, maxMassas
+    real, intent(inout) :: posicoes(:,:), momentos(:,:), massas(:)
+
+    ! gera os valores
+    ! gerarValores(N, massas, posicoes, momentos, minMassas, maxMassas, minPos, maxPos, minMom, maxMom)
+
+    ! zera o momento angular
+
+    ! zera o centro de massas
+
+    ! zera o momento linear
+
+    ! zera a energia total
+
+
+  end subroutine condicionar
+
+end module condicoesArtigo
